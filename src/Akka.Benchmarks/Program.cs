4f4229965fde0e90fc64271a095420bc5ac47f67
alexvaluyskiy <yurisikorsky@gmail.com>
2017-07-12 19:32:16 +0300

    Added ActorPath benchmarks

---
+++ src/Akka.Benchmarks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace Akka.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<ActorPathBenchmarks>();
+
+            Console.ReadLine();
+        }
+    }
+}
