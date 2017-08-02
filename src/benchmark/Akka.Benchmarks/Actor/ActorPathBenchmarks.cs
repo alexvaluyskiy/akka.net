7680a997e1f61b9cce3c80643ac2f587c6475969
alexvaluyskiy <yurisikorsky@gmail.com>
2017-08-02 16:38:33 +0300

    More Actorpath benchmarks

--- src/benchmark/Akka.Benchmarks/Actor/ActorPathBenchmarks.cs
+++ src/benchmark/Akka.Benchmarks/Actor/ActorPathBenchmarks.cs
@@ -9,26 +9,30 @@ namespace Akka.Benchmarks.Actor
     [BenchmarkCategory("actor")]
     public class ActorPathBenchmarks
     {
-        private static readonly ActorPath TestActorPath = ActorPath.Parse("akka.tcp://Sys@localhost:9091/user/foo");
-        private static readonly RootActorPath RootAddress = new RootActorPath(Address.AllSystems);
+        private ActorPath TestActorPath = ActorPath.Parse("akka.tcp://Sys@localhost:9091/user/foo");
+        private RootActorPath TestRootAddress = ActorPath.Parse("akka.tcp://Sys@localhost:9091/user/foo").Root as RootActorPath;
+        private string TestLocalAddressString = "akka://Sys/user/foo";
+        private string TestRemoteAddressString = "akka.tcp://Sys@localhost:9091/user/foo";
+        private IEnumerable<string> TestActorElements = new List<string>() { "user", "foo" };
+        private Address TestAddress = new Address("akka.tcp", "Sys", "localhost", 9091);
 
         [Benchmark]
         public ActorPath ActorPath_Parse_local_address()
         {
-            return ActorPath.Parse("akka://Sys/user/foo");
+            return ActorPath.Parse(TestLocalAddressString);
         }
 
         [Benchmark]
         public ActorPath ActorPath_Parse_remote_address()
         {
-            return ActorPath.Parse("akka.tcp://Sys@localhost:9091/user/foo");
+            return ActorPath.Parse(TestRemoteAddressString);
         }
 
         [Benchmark]
         public ActorPath ActorPath_TryParse_local_address()
         {
             ActorPath target;
-            ActorPath.TryParse("akka://Sys/user/foo", out target);
+            ActorPath.TryParse(TestLocalAddressString, out target);
             return target;
         }
 
@@ -36,7 +40,7 @@ namespace Akka.Benchmarks.Actor
         public ActorPath ActorPath_TryParse_remote_address()
         {
             ActorPath target;
-            ActorPath.TryParse("akka.tcp://Sys@localhost:9091/user/foo", out target);
+            ActorPath.TryParse(TestRemoteAddressString, out target);
             return target;
         }
 
@@ -44,7 +48,7 @@ namespace Akka.Benchmarks.Actor
         public Address ActorPath_TryParseAddress_local_address()
         {
             Address target;
-            ActorPath.TryParseAddress("akka://Sys/user/foo", out target);
+            ActorPath.TryParseAddress(TestLocalAddressString, out target);
             return target;
         }
 
@@ -52,32 +56,56 @@ namespace Akka.Benchmarks.Actor
         public Address ActorPath_TryParseAddress_remote_address()
         {
             Address target;
-            ActorPath.TryParseAddress("akka.tcp://Sys@localhost:9091/user/foo", out target);
+            ActorPath.TryParseAddress(TestRemoteAddressString, out target);
             return target;
         }
 
         [Benchmark]
-        public IReadOnlyList<string> ActorPath_Elements()
+        public ActorPath ActorPath_Concat_operator()
         {
-            return TestActorPath.Elements;
+            return (TestRootAddress / "user" / "foo");
         }
 
         [Benchmark]
-        public ActorPath ActorPath_Concat_Operator()
+        public ActorPath ActorPath_Concat_operator_with_enumerable()
         {
-            return (RootAddress / "user" / "foo");
+            return (TestRootAddress / TestActorElements);
         }
-		
-		[Benchmark]
+
+        [Benchmark]
         public string ActorPath_ToStringWithAddress()
         {
             return TestActorPath.ToStringWithAddress();
         }
-		
-		[Benchmark]
+
+        [Benchmark]
+        public string ActorPath_ToStringWithoutAddress()
+        {
+            return TestActorPath.ToStringWithoutAddress();
+        }
+
+        [Benchmark]
+        public string ActorPath_ToStringWithUid()
+        {
+            return TestActorPath.ToStringWithUid();
+        }
+
+        [Benchmark]
+        public ActorPath ActorPath_Child()
+        {
+            return TestActorPath.Child("foo");
+        }
+
+        [Benchmark]
         public string ActorPath_ToSerializationFormat()
         {
             return TestActorPath.ToSerializationFormat();
+        }
+
+        [Benchmark]
+        public string ActorPath_ToSerializationFormatWithAddress()
+        {
+            return TestActorPath.ToSerializationFormatWithAddress(TestAddress);
         }
 
         [Benchmark]
@@ -90,6 +118,54 @@ namespace Akka.Benchmarks.Actor
         public int ActorPath_GetHashCode()
         {
             return TestActorPath.GetHashCode();
+        }
+
+        [Benchmark]
+        public IReadOnlyList<string> RootActorPath_Elements()
+        {
+            return TestRootAddress.Elements;
+        }
+
+        [Benchmark]
+        public IReadOnlyList<string> ChildActorPath_Elements()
+        {
+            return TestActorPath.Elements;
+        }
+
+        [Benchmark]
+        public IReadOnlyList<string> RootActorPath_ElementsWithUid()
+        {
+            return TestRootAddress.ElementsWithUid;
+        }
+
+        [Benchmark]
+        public IReadOnlyList<string> ChildActorPath_ElementsWithUid()
+        {
+            return TestActorPath.ElementsWithUid;
+        }
+
+        [Benchmark]
+        public ActorPath RootActorPath_Root()
+        {
+            return TestRootAddress.Root;
+        }
+
+        [Benchmark]
+        public ActorPath ChildActorPath_Root()
+        {
+            return TestActorPath.Root;
+        }
+
+        [Benchmark]
+        public int RootActorPath_CompareTo()
+        {
+            return TestRootAddress.CompareTo(TestRootAddress);
+        }
+
+        [Benchmark]
+        public int ChildActorPath_CompareTo()
+        {
+            return TestActorPath.CompareTo(TestActorPath);
         }
     }
 }
