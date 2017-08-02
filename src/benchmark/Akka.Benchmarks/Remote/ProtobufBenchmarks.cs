e0866e18695874244756eb9ad6622d9677a2fed7
alexvaluyskiy <yurisikorsky@gmail.com>
2017-08-02 11:27:50 +0300

    protobuf benchmarks

---
+++ src/benchmark/Akka.Benchmarks/Remote/ProtobufBenchmarks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Akka.Configuration;
+using BenchmarkDotNet.Attributes;
+using Google.Protobuf;
+
+namespace Akka.Benchmarks.Remote
+{
+    [Config(typeof(MyConfig))]
+    [MemoryDiagnoser]
+    [BenchmarkCategory("serialization")]
+    public class ProtobufBenchmarks
+    {
+        private byte[] TestIntegerBytes = Convert.FromBase64String("xwEAAA==");
+
+        [Benchmark]
+        public ByteString ByteString_CopyFrom()
+        {
+            return ByteString.CopyFrom(TestIntegerBytes);
+        }
+
+        [Benchmark]
+        public ByteString ByteString_CopyFromUtf8()
+        {
+            return ByteString.CopyFromUtf8("test");
+        }
+    }
+}
